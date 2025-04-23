2d3572001fd08e003ae857975a0e6475b21ce795
HaydenDArmstrong <157652748+HaydenDArmstrong@users.noreply.github.com>
2025-04-23 16:39:09 -0600

    Update Amotor.ino

--- ArduinoSide/Amotor.ino
+++ ArduinoSide/Amotor.ino
@@ -1,8 +1,6 @@
-void Motor(int Dir, int Speed1, int Speed2, int Speed3, int Speed4) {
-  analogWrite(PWM2A, Speed1);  // Motor PWM speed regulation
-  analogWrite(PWM2B, Speed2);  // Motor PWM speed regulation
-  analogWrite(PWM0A, Speed3);  // Motor PWM speed regulation
-  analogWrite(PWM0B, Speed4);  // Motor PWM speed regulation
+void Motor(int Dir, int SpeedL, int SpeedR) {
+   analogWrite(PWML, SpeedL);  // Motor speed regulation from consts speedL and speedR in arduinoside.ino
+  analogWrite(PWMR, SpeedR);  
 
   digitalWrite(DIR_LATCH, LOW);            // DIR_LATCH sets the low level and writes the direction of motion in preparation
   shiftOut(DATA, DIR_CLK, MSBFIRST, Dir);  // Write Dir motion direction value
