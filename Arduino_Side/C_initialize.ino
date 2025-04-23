a5644a00d9df6e35500be6f25aa57915b1543c97
HaydenDArmstrong <157652748+HaydenDArmstrong@users.noreply.github.com>
2025-04-23 17:23:05 -0600

    Update C_initialize.ino

--- Arduino_Side/C_initialize.ino
+++ Arduino_Side/C_initialize.ino
@@ -1,20 +1,28 @@
-
-
-
 void initial() {
   Serial.println("Initializing system components...");
   delay(100);
 
   // Test servo
   Serial.print("Testing servo... ");
-  headServo.attach(headServoPin);
-  headServo.write(90); // Move to center position
+  LRServo.attach(LRServoPin);
+  LRServo.write(90); // Move to center position
   delay(500);
-  headServo.write(0); // Move to left position
+  LRServo.write(0); // Move to left position
   delay(500);
-  headServo.write(180); // Move to right position
+  LRServo.write(180); // Move to right position
   delay(500);
-  headServo.write(90); // Reset to center
+  LRServo.write(90); // Reset to center
+  Serial.println("Servo OK.");
+
+  Serial.print("Testing servo up down... ");
+  UDServo.attach(UDServoPin);
+  UDServo.write(90); // Move to center position
+  delay(500);
+  UDServo.write(30); // Move to left position
+  delay(500);
+  UDServo.write(180); // Move to right position
+  delay(500);
+  UDServo.write(90); // Reset to center
   Serial.println("Servo OK.");
 
   // // Test laser detector
@@ -30,51 +38,28 @@ void initial() {
 
   // Test motors
   Serial.print("Testing motors... ");
-  Motor(Move_Forward, 150, 150, 150, 150); // Run motors at half speed forward
+  Motor(aFwd, 150, 150); // Run motors at half speed forward
   delay(500);
-  Motor(Stop, 0, 0, 0, 0); // Stop motors
+  Motor(Stop, 0, 0);
   Serial.println("Motors OK.");
 
-  Serial.println("System initialization complete.");
-  hasInitialized = true; // Mark system as initialized
+   hasInitialized = true; // Mark system as initialized
 
-  // Print a dashed line
+
   for (int i = 0; i < 50; i++) {
     Serial.print('-');
   }
-  Serial.println(); // Move to the next line
+  Serial.println(); 
 }
-
-void displayStats() {
-  // Get and display the current laser status
-  // int laserDetected = digitalRead(laserReceiverLeftPin) || digitalRead(laserReceiverRightPin);
-  // Serial.println(laserDetected);
-  // if (laserDetected == LOW) {
-  //   Serial.println("Laser detected: Yes");
-  // } else {
-  //   Serial.println("Laser detected: No");
-  // }
-
-  // Print lives
-  Serial.print("Lives remaining: ");
-  Serial.println(lives);
-
-  // Print a separator line
-  for (int i = 0; i < 50; i++) {
-    Serial.print('-');
-  }
-  Serial.println();
-}
-
 
 
 void moveServoHead() {
-  headServo.write(90); // Move to center position
+  LRServo.write(90); // Move to center position
   delay(1000);
-  headServo.write(45); // Move to left position
+  LRServo.write(45); // Move to left position
   delay(1000);
-  headServo.write(135); // Move to right position
+  LRServo.write(135); // Move to right position
   delay(1000);
-  headServo.write(90); // Reset to center
+  LRServo.write(90); // Reset to center
   delay(1000);
 }
